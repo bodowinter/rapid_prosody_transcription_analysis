4a087e9b5c5b283d75cc61a9dbe4bfc2e1381b54
bodowinter <bodo@bodowinter.com>
2016-07-29 10:20:27 +0200

    added cluster analysis to individual difference analysis

--- 006_by_individual_analysis.R
+++ 006_by_individual_analysis.R
@@ -4,6 +4,15 @@
 ## July 27, 2016: Finishing brushes and incorporation of spectral tilt
 ## July 27, 2016: Replaced ggplot2 with base graphs
 
+##------------------------------------------------------------------
+## Load in data and packages + preprocessing:
+##------------------------------------------------------------------
+
+## Load in libraries:
+
+library(cluster)
+library(pvclust)
+
 ## Load mixed models:
 
 mainDir <- '/Users/teeniematlock/Desktop/research/rapid_prosody_transcription/analysis/data'
@@ -23,23 +32,24 @@ allsubs <- data.frame(Subject = rownames(coef(xmdl.MeanPitch)$Listener),
 	Slope = coef(xmdl.SlopeST)$Listener[, 2], 
 	Freq = coef(xmdl.Freq)$Listener[, 2], 
 	Vowel = coef(xmdl.Vowel)$Listener[, 2], 
-	POS = coef(xmdl.POS_class)$Listener[, 2], 			# for content
+	POS = coef(xmdl.POS)$Listener[, 2], 			# for content
 	Focused = coef(xmdl.Focused)$Listener[, 2], 			# for focus particle
 	Argument = coef(xmdl.argument)$Listener[, 2],			# for last argument
 	SpectralEmphasis = coef(xmdl.SpectralEmphasis)$Listener[, 2],
 	H1A2 = coef(xmdl.H1A2)$Listener[, 2],
-	H1A3 = coef(xmdl.H1A3)$Listener[, 2]
+	H1A3 = coef(xmdl.H1A3)$Listener[, 2],
+	Accented = coef(xmdl.Accented)$Listener[, 2]
 	)
-
-## Hand-code accenteds:
-
-accenteds <- coef(xmdl.Accented.bobyqa)$Listener
-allsubs$Accented <- accenteds$Accented_cyes - accenteds$Accented_cno
 
 ## Add mean absolute change for the multi-level factors:
 
-allsubs$AccentType <- rowMeans(abs(coef(xmdl.AccentType.bobyqa)$Listener[, 1:4]))
-allsubs$AccentPosition <- rowMeans(abs(coef(xmdl.AccentPosition.bobyqa)$Listener[, c(1, 3, 4)]))
+allsubs$AccentType <- rowMeans(abs(coef(xmdl.AccentType)$Listener[, 1:4]))
+allsubs$AccentPosition <- rowMeans(abs(coef(xmdl.AccentPosition)$Listener[, c(1:3)]))
+
+
+##------------------------------------------------------------------
+## Correlations between random effects slopes:
+##------------------------------------------------------------------
 
 ## Generate correlation matrices:
 
@@ -108,5 +118,64 @@ cor.test(subsphon$Duration, subsphon$Spectrum)
 dunnsidak(cor.test(subsphon$Pitch, subsphon$Spectrum)$p.val, 3)
 dunnsidak(cor.test(subsphon$Pitch, subsphon$Duration)$p.val, 3)
 dunnsidak(cor.test(subsphon$Duration, subsphon$Spectrum)$p.val, 3)
+
+
+
+##------------------------------------------------------------------
+## Cluster analysis following Levshina (2016, Ch. 5):
+##------------------------------------------------------------------
+
+## Create a hierarchical agglomerative cluster model:
+
+slope.dist <- allsubs[, -1]
+rownames(slope.dist) <- allsubs$Subject
+slope.dist <- dist(slope.dist, method = 'euclidian')
+slope.hc <- hclust(slope.dist, method = 'ward.D2')
+
+## Test Silhouette width of different cluster solutions:
+
+asw <- sapply(2:10,
+	function(x) summary(silhouette(cutree(slope.hc, k = x), slope.dist))$avg.width)
+asw		# three cluster solution is best
+
+## Plot this with subject names:
+
+quartz('', 11, 6)
+plot(slope.hc, hang = -1)
+rect.hclust(slope.hc, k = 3)
+
+## Get cluster affiliations:
+
+allsubs_clust <- cbind(allsubs,
+	Cluster = cutree(slope.hc, k = 3))[, -1]
+allsubs_clust <- split(allsubs_clust, allsubs_clust$Cluster)
+clust_sum <- lapply(allsubs_clust, FUN = colMeans)
+clust_sum <- as.data.frame(clust_sum)
+clust_sum <- clust_sum[-nrow(clust_sum), ]
+names(clust_sum) <- paste0('Cluster', 1:3)
+
+## How do cluster 1 and cluster 2 differ?
+
+diffs <- clust_sum$Cluster2 - clust_sum$Cluster1
+names(diffs) <- rownames(clust_sum)
+# cluster 2 pays more attention to: frequency, POS, focus particle and last argument
+# much less to Accented
+
+## Validate cluster solution:
+
+this_df <- allsubs[, -1]
+rownames(this_df) <- allsubs$Subject
+
+set.seed(42)
+slope.pvc <- pvclust(t(this_df),
+	method.hclust = 'ward.D2', method.dist = 'euclidian')
+
+## Visualize this with clusters that surpass a = 0.05:
+
+quartz('', 11, 6)
+plot(slope.pvc, hang = -1)
+pvrect(slope.pvc, alpha = 0.95)
+
+
 
 
